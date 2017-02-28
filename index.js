856b409ecdb38e5f6a1ad4c1f0f31dbd1e174002
Cory <cory.forsyth@gmail.com>
2017-02-27 20:16:18 -0500

    add debug headers and secret headers pages

--- index.js
+++ index.js
@@ -8,7 +8,22 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
+  console.log('req.headers',req.headers)
   res.render('pages/index')
+});
+
+app.get('/debug/headers', (req, res) => {
+  let headers = req.headers;
+  res.render('pages/debug-headers', {headers})
+});
+
+app.get('/quiz/lang', (req, res) => {
+  let header = req.headers['x-secret-code']
+  if (header && header.toLowerCase().includes('pineapple')) {
+    res.render('pages/quiz-lang', {correct: true})
+  } else {
+    res.render('pages/quiz-lang', {correct: false})
+  }
 });
 
 app.listen(app.get('port'), function() {
